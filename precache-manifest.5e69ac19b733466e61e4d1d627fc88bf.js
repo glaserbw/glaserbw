9b306b5d19583b1fb5abb19345c604dc858f80a3
Brian Glaser <glaserbw@gmail.com>
2018-11-06 14:41:01 -0800

    Updates

---
+++ precache-manifest.5e69ac19b733466e61e4d1d627fc88bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fb8ea705c621730235c17f00e1a03b7",
+    "url": "/static/media/stokeyswayhome.2fb8ea70.png"
+  },
+  {
+    "revision": "21c797fb75283453daa54b72ac218a9c",
+    "url": "/static/media/stalkystocks.21c797fb.png"
+  },
+  {
+    "revision": "9da3c8f6b0c019a9853e042cce5980e4",
+    "url": "/static/media/saltandpepperfam.9da3c8f6.png"
+  },
+  {
+    "revision": "f769f0e30713b1629efc174ba5133230",
+    "url": "/static/media/kainan.f769f0e3.png"
+  },
+  {
+    "revision": "bb65b03226194cc44bbd22eb9b267c4e",
+    "url": "/static/media/brian.bb65b032.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d3a50183075ee35a597",
+    "url": "/static/js/main.3d3a5018.chunk.js"
+  },
+  {
+    "revision": "6a653a538a7022c5a50d",
+    "url": "/static/js/1.6a653a53.chunk.js"
+  },
+  {
+    "revision": "3d3a50183075ee35a597",
+    "url": "/static/css/main.040fc805.chunk.css"
+  },
+  {
+    "revision": "8baac1e66267e0a000c72122b891baf5",
+    "url": "/index.html"
+  }
+];
